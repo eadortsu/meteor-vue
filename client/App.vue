d53baf371bafad826383449c3bbac7f6c2b8e266
eadortsu <mail@eadortsu.com>
2019-10-16 17:41:06 +0000

    Added Vue,Vuetify.

---
+++ client/App.vue
@@ -0,0 +1,360 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer
+                v-model="drawer"
+                :clipped="$vuetify.breakpoint.lgAndUp"
+                app
+        >
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-row
+                            v-if="item.heading"
+                            :key="item.heading"
+                            align="center"
+                    >
+                        <v-col cols="6">
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-col>
+                        <v-col
+                                cols="6"
+                                class="text-center"
+                        >
+                            <a
+                                    href="#!"
+                                    class="body-2 black--text"
+                            >EDIT</a>
+                        </v-col>
+                    </v-row>
+                    <v-list-group
+                            v-else-if="item.children"
+                            :key="item.text"
+                            v-model="item.model"
+                            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                            append-icon=""
+                    >
+                        <template v-slot:activator>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ item.text }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                        <v-list-item
+                                v-for="(child, i) in item.children"
+                                :key="i"
+                                @click=""
+                        >
+                            <v-list-item-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ child.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                    <v-list-item
+                            v-else
+                            :key="item.text"
+                            @click=""
+                    >
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{ item.text }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+                :clipped-left="$vuetify.breakpoint.lgAndUp"
+                app
+                color="blue darken-3"
+                dark
+        >
+            <v-toolbar-title
+                    style="width: 300px"
+                    class="ml-0 pl-4"
+            >
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                <span class="hidden-sm-and-down">Mest APP</span>
+            </v-toolbar-title>
+            <v-text-field
+                    flat
+                    solo-inverted
+                    hide-details
+                    prepend-inner-icon="mdi-magnify"
+                    label="Search"
+                    v-model="search"
+                    class="hidden-sm-and-down"
+            ></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>mdi-apps</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-bell</v-icon>
+            </v-btn>
+            <v-btn
+                    icon
+                    large
+            >
+                <v-avatar
+                        size="32px"
+                        item
+                >
+                    <v-img
+                            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+                            alt="Vuetify"
+                    >
+                    </v-img></v-avatar>
+            </v-btn>
+        </v-app-bar>
+        <v-content>
+            <v-card>
+               <!-- <v-card-title>
+                    EIT 2020
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                            v-model="search"
+                            append-icon="mdi-magnify"
+                            label="Search"
+                            single-line
+                            hide-details
+                    ></v-text-field>
+                </v-card-title>-->
+
+                <v-data-table
+                        v-model="selected"
+                        :headers="headers"
+                        :items="desserts"
+                        :search="search"
+                        item-key="name"
+                        show-select
+
+                >
+                    <template v-slot:top>
+                        <v-toolbar flat color="white">
+                            <v-toolbar-title>EIT's</v-toolbar-title>
+                            <v-divider
+                                    class="mx-4"
+                                    inset
+                                    vertical
+                            ></v-divider>
+                            <v-spacer></v-spacer>
+                            <v-dialog v-model="dialog" max-width="500px">
+                                <template v-slot:activator="{ on }">
+                                    <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>
+                                        <span class="headline">{{ formTitle }}</span>
+                                    </v-card-title>
+
+                                    <v-card-text>
+                                        <v-container>
+                                            <v-row>
+                                                <v-col cols="6" sm="6" md="6">
+                                                    <v-text-field v-model="editedItem.name" label="First Name"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="6" sm="6" md="6">
+                                                    <v-text-field v-model="editedItem.name" label="Last Name"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" sm="12" md="12">
+                                                    <v-text-field v-model="editedItem.country" label="Country"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" sm="12" md="12">
+                                                    <v-text-field v-model="editedItem.email" label="Email"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" sm="12" md="12">
+
+                                                    <v-text-field v-model="editedItem.contact" label="Contact"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" sm="12" md="12">
+                                                    <v-text-field v-model="editedItem.age" label="Age"></v-text-field>
+                                                </v-col>
+                                                <v-col cols="12" sm="12" md="12">
+                                                    <v-select :items="genders" v-model="editedItem.gender" label="Gender"></v-select>
+
+                                                </v-col>
+                                            </v-row>
+                                        </v-container>
+                                    </v-card-text>
+
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                                        <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-toolbar>
+                    </template>
+                    <template v-slot:item.action="{ item }">
+                        <v-icon
+                                small
+                                class="mr-2"
+                                @click="editItem(item)"
+                        >
+                            mdi-pencil
+                        </v-icon>
+                        <v-icon
+                                small
+                                @click="deleteItem(item)"
+                        >
+                            mdi-delete
+                        </v-icon>
+                    </template>
+                    <template v-slot:no-data>
+                        <v-btn color="primary" @click="initialize">Reset</v-btn>
+                    </template>
+                </v-data-table>
+
+            </v-card>
+
+        </v-content>
+        <v-btn
+                bottom
+                color="pink"
+                dark
+                fab
+                fixed
+                right
+                @click="dialog = !dialog"
+        >
+            <v-icon>mdi-plus</v-icon>
+        </v-btn>
+
+    </v-app>
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+        data: () => ({
+            genders: ['Male','Female'],
+            dialog: false,
+            drawer: null,
+            items: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard' },
+                {
+                    icon: 'mdi-chevron-up',
+                    'icon-alt': 'mdi-chevron-down',
+                    text: "EIT's",
+                    model: false,
+                    children: [
+                        { icon:"mdi-account-multiple", text: 'Show All' },
+                        { icon:"mdi-account-multiple-plus", text: 'Add New' },
+
+
+                    ],
+                },
+
+
+            ],
+            search: '',
+            selected: [],
+            headers: [
+                {
+                    text: 'Full Name',
+                    align: 'left',
+                    sortable: true,
+                    value: 'name',
+                },
+                { text: 'Country', value: 'country' },
+                { text: 'Email', value: 'email' },
+                { text: 'Contact', value: 'contact' },
+                { text: 'Age', value: 'age' },
+                { text: 'Gender', value: 'gender' },
+                { text: 'Actions', value: 'action', sortable: false },
+            ],
+            desserts: [],
+            editedIndex: -1,
+            editedItem: {
+                name: '',
+                calories: 0,
+                fat: 0,
+                carbs: 0,
+                protein: 0,
+            },
+            defaultItem: {
+                name: '',
+                calories: 0,
+                fat: 0,
+                carbs: 0,
+                protein: 0,
+            },
+        }),
+
+        computed: {
+            formTitle () {
+                return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+            },
+        },
+
+        watch: {
+            dialog (val) {
+                val || this.close()
+            },
+        },
+
+        created () {
+            this.initialize()
+        },
+
+        methods: {
+            initialize () {
+                this.desserts = [
+                    {
+                        name: 'Eugene Ador',
+                        country: 'Ghana',
+                        email: 'eugene.adortsu@meltwater.org',
+                        contact: '+233505652156',
+                        age: 65,
+                        gender: 'Male',
+                    },
+                ]
+            },
+
+            editItem (item) {
+                this.editedIndex = this.desserts.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialog = true
+            },
+
+            deleteItem (item) {
+                const index = this.desserts.indexOf(item)
+                confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+            },
+
+            close () {
+                this.dialog = false
+                setTimeout(() => {
+                    this.editedItem = Object.assign({}, this.defaultItem)
+                    this.editedIndex = -1
+                }, 300)
+            },
+
+            save () {
+                if (this.editedIndex > -1) {
+                    Object.assign(this.desserts[this.editedIndex], this.editedItem)
+                } else {
+                    this.desserts.push(this.editedItem)
+                }
+                this.close()
+            },
+        },
+    }
+</script>
